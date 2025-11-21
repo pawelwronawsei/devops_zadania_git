c5fe52b813dc5b2e3badd2c352ee17e0266e31a0
pawelwronawsei <146821537+pawelwronawsei@users.noreply.github.com>
2025-11-21 15:25:20 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+const app = express();
+
+app.get('/api/time', async (req, res) => {
+  try {
+    const conn = await mysql.createConnection({
+      host: 'mysql-db',
+      user: 'root',
+      password: 'rootpassword',
+      database: 'mydb'
+    });
+
+    const [rows] = await conn.execute('SELECT NOW() AS time');
+
+    res.json({ status: 'ok', time: rows[0].time });
+
+    await conn.end();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => console.log('Backend działa na porcie 3000'));
