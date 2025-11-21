9b09d4d9fa4fcbb68be3dd7d4776c90d222c1166
pawelwronawsei <146821537+pawelwronawsei@users.noreply.github.com>
2025-11-21 16:04:14 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.get('/api/time', async (req, res) => {
   try {
